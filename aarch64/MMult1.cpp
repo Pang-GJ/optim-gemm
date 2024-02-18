c97623e75af7ad87407714d209614bb5606d10f4
Pang-GJ <2362421192@qq.com>
2024-02-18 21:07:52 +0800

    add MMult1

---
+++ aarch64/MMult1.cpp
@@ -0,0 +1,28 @@
+/* Routine for computing C = A * B */
+
+void AddDot(int, float*, float*, int, float*);
+
+void MY_MMult(int m, int n, int k, float* a, int lda, float* b, int ldb,
+              float* c, int ldc) {
+  int i, j;
+
+#define A(i, j) a[(i) * k + (j)]
+#define B(i, j) b[(i) * n + (j)]
+#define C(i, j) c[(i) * n + (j)]
+
+  for (int j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++) {
+      AddDot(k, &A(i, 0), &B(0, j), ldb, &C(i, j));
+    }
+  }
+
+#undef A
+#undef B
+#undef C
+}
+
+void AddDot(int k, float* x, float* y, int ldb, float* gamma) {
+  for (int p = 0; p < k; p++) {
+    *gamma += x[p] * y[p * ldb];
+  }
+}
